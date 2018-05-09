a26fdabd4703fa8377ccb7c16c5b42040ad0ca85
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-05-09 14:15:59 +0200

    Add indexed map test and couple of fixes
    * Remove type vars from tasty types
    * Fix impure by name arguments to macros

--- tests/run/tasty-indexed-map/quoted_1.scala
+++ tests/run/tasty-indexed-map/quoted_1.scala
@@ -13,17 +13,32 @@ object MyMap {
   def create[Keys](implicit s: Size[Keys]): MyMap[Keys] = new MyMap[Keys](new Array[Int](s.size))
 }
 
-trait Size[Keys] { def size: Int }
+class Size[HList](val size: Int) extends AnyVal
 object Size {
-  def apply[Keys](v: Int): Size[Keys] = new Size { def size = v }
-  implicit val unit: Size[Unit] = Size(0)
-  implicit def cons[H, T](implicit s: Size[T]): Size[(H, T)] = Size(s.size + 1)
+  type HList = Cons[_, _] | Unit
+  type Cons[H, T <: HList] = (H, T)
+
+  implicit inline def getSize[HL <: HList]: Size[HL] = ~sizeImpl(Universe.compilationUniverse)('[HL])
+
+  def sizeImpl[HL <: HList](u: Universe)(implicit keys: Type[HL]): Expr[Size[HL]] = {
+    import u._
+    import u.tasty._
+
+    def sizeOf(tp: TypeOrBounds): Int = tp match {
+      case Type.AppliedType(_, h :: t :: Nil) => 1 + sizeOf(t)
+      case _ => 0
+    }
+
+    val size = sizeOf(keys.toTasty.tpe)
+
+    '(new Size(~size.toExpr))
+  }
 }
 
 class Index[K, Keys](val index: Int) extends AnyVal
 object Index {
 
-  implicit def zero[K, T]: Index[K, (K, T)] = new Index(0)
+  implicit inline def zero[K, T]: Index[K, (K, T)] = new Index(0)
 
   implicit inline def succ[K, H, T](implicit prev: => Index[K, T]): Index[K, (H, T)] = ~succImpl(Universe.compilationUniverse)('[K], '[H], '[T])
 
