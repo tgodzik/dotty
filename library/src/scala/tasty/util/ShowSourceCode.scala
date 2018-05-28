0b74a53920749da140a55537a69b3275f5dcdf7b
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-05-28 08:52:01 +0200

    wip

--- library/src/scala/tasty/util/ShowSourceCode.scala
+++ library/src/scala/tasty/util/ShowSourceCode.scala
@@ -4,13 +4,6 @@ package util
 class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty0) {
   import tasty._
 
-  private[this] var indent: Int = 0
-  def indented(printIndented: => Unit): Unit = {
-    indent += 1
-    printIndented
-    indent -= 1
-  }
-
   def showTree(tree: Tree)(implicit ctx: Context): String =
     (new Buffer).printTree(tree).result()
 
@@ -24,266 +17,271 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
     (new Buffer).printConstant(const).result()
 
   private class Buffer(implicit ctx: Context) {
-    self =>
 
     private val sb: StringBuilder = new StringBuilder
 
+    private[this] var indent: Int = 0
+    def indented(printIndented: => Unit): Unit = {
+      indent += 1
+      printIndented
+      indent -= 1
+    }
+
     def result(): String = sb.result()
 
-    private def lineBreak: String = "\n" + ("  " * indent)
+    def lineBreak: String = "\n" + ("  " * indent)
 
-    def printTree(tree: Tree): Buffer = {
-      tree match {
-        case tree@PackageClause(Term.Ident(name), stats) =>
-          val stats1 = stats.collect {
-            case stat@Definition() if !(stat.flags.isObject && stat.flags.isLazy) => stat
-            case stat@Import(_, _) => stat
-          }
+    def printTree(tree: Tree): Buffer = tree match {
+      case tree@PackageClause(Term.Ident(name), stats) =>
+        val stats1 = stats.collect {
+          case stat@Definition() if !(stat.flags.isObject && stat.flags.isLazy) => stat
+          case stat@Import(_, _) => stat
+        }
 
-          if (name == "<empty>") {
+        if (name == "<empty>") {
+          printTrees(stats1, lineBreak)
+        } else {
+          this += "package " += name += " {"
+          indented {
+            this += lineBreak
             printTrees(stats1, lineBreak)
-          } else {
-            this += "package " += name += " {"
-            indented {
-              this += lineBreak
-              printTrees(stats1, lineBreak)
-            }
-            this += lineBreak += "}" += lineBreak
           }
+          this += lineBreak += "}" += lineBreak
+        }
 
-        case cdef@ClassDef(name, DefDef(_, targs, argss, _, _), parents, self, stats) =>
-          val flags = cdef.flags
-          if (flags.isCase) this += "case "
+      case cdef@ClassDef(name, DefDef(_, targs, argss, _, _), parents, self, stats) =>
+        val flags = cdef.flags
+        if (flags.isCase) this += "case "
 
-          if (cdef.flags.isObject) this += "object " += name.stripSuffix("$")
-          else this += "class " += name
+        if (cdef.flags.isObject) this += "object " += name.stripSuffix("$")
+        else this += "class " += name
 
-          if (!cdef.flags.isObject) {
-            printTargsDefs(targs)
-            val it = argss.iterator
-            while (it.hasNext)
-              printArgsDefs(it.next())
-          }
-
-          val parents1 = parents.filter {
-            case Term.Apply(Term.Select(Term.New(tpt), _, _), _) if Types.isJavaLangObject(tpt.tpe) => false
-            case TypeTree.TypeSelect(Term.Select(Term.Ident("_root_"), "scala", _), "Product") => false
-            case _ => true
-          }
-          if (parents1.nonEmpty) {
-            sb.append(" extends")
-            parents1.foreach {
-              case parent@Term.Apply(Term.Select(Term.New(tpt), _, _), args) =>
-                this += " "
-                printTypeTree(tpt)
-                if (args.nonEmpty) {
-                  this += "("
-                  printTrees(args, ", ")
-                  this += ")"
-                }
-
-              case parent@TypeTree() =>
-                sb.append(" ")
-                printTypeTree(parent)
-            }
-          }
-
-          val stats1 = stats.collect {
-            case stat@Definition() if !stat.flags.isParam && !stat.flags.isParamAccessor => stat
-            case stat@Import(_, _) => stat
-            case stat@Term() => stat
-          }
-          if (stats1.nonEmpty) {
-            this += " {"
-            indented {
-              this += lineBreak
-              printTrees(stats1, lineBreak)
-            }
-            this += lineBreak += "}"
-          }
-
-        case tdef@TypeDef(name, rhs) =>
-          this += "type "
-          printTargDef(tdef)
-
-        case vdef@ValDef(name, tpt, rhs) =>
-          val flags = vdef.flags
-          if (flags.isOverride) this += "override "
-
-          if (flags.isLazy) this += "lazy "
-          if (vdef.flags.isMutable) this += "var "
-          else this += "val "
-
-          this += name += ": "
-          printTypeTree(tpt)
-          rhs match {
-            case Some(tree) =>
-              this += " = "
-              printTree(tree)
-            case None =>
-          }
-
-        case ddef@DefDef(name, targs, argss, tpt, rhs) =>
-          val flags = ddef.flags
-          if (flags.isOverride) sb.append("override ")
-
-          this += "def " += name
+        if (!cdef.flags.isObject) {
           printTargsDefs(targs)
           val it = argss.iterator
           while (it.hasNext)
             printArgsDefs(it.next())
-          this += ": "
-          printTypeTree(tpt)
-          rhs match {
-            case Some(tree) =>
-              this += " = "
-              printTree(tree)
-            case None =>
-          }
+        }
 
-        case tree@Term.Ident(name) =>
-          printType(tree.tpe)
-
-        case Term.Select(qual, name, sig) =>
-          printTree(qual)
-          if (name != "<init>")
-            this += "." += name
-
-        case Term.Literal(const) =>
-          printConstant(const)
-
-        case Term.This(id) =>
-          this += "this" // TODO add id
-
-        case Term.New(tpt) =>
-          this += "new "
-          printTypeTree(tpt)
-
-        case Term.NamedArg(name, arg) =>
-          ???
-
-        case SpecialOp("throw", expr :: Nil) =>
-          this += "throw "
-          printTree(expr)
-
-        case Term.Apply(fn, args) =>
-          printTree(fn)
-          this += "("
-          printTrees(args, ", ")
-          this += ")"
-
-        case Term.TypeApply(fn, args) =>
-          printTree(fn)
-          this += "["
-          printTypeTrees(args, ", ")
-          this += "]"
-
-        case Term.Super(qual, tptOpt) =>
-          ???
-
-        case Term.Typed(term, tpt) =>
-          this += "("
-          printTree(term)
-          this += ": "
-          printTypeTree(tpt)
-          this += ")"
-
-        case Term.Assign(lhs, rhs) =>
-          printTree(lhs)
-          this += " = "
-          printTree(rhs)
-
-        case Term.Block(stats, expr) =>
-          expr match {
-            case Term.Lambda(_, _) =>
-              // Decompile lambda from { def annon$(...) = ...; closure(annon$, ...)}
-              val DefDef(_, _, args :: Nil, _, Some(rhs)) :: Nil = stats
-              this += "("
-              printArgsDefs(args)
-              this += " => "
-              printTree(rhs)
-              this += ")"
-
-            case Term.Apply(Term.Ident("while$"), _) =>
-              val DefDef("while$", _, _, _, Some(Term.If(cond, Term.Block(body :: Nil, _), _))) = stats.head
-              this += "while ("
-              printTree(cond)
-              this += ") "
-              printTree(body)
-
-            case Term.Apply(Term.Ident("doWhile$"), _) =>
-              val DefDef("doWhile$", _, _, _, Some(Term.Block(List(body), Term.If(cond, _, _)))) = stats.head
-              this += "do "
-              printTree(body)
-              this += " while ("
-              printTree(cond)
-              this += ")"
-
-            case _ =>
-              this += "{"
-              indented {
-                if (!stats.isEmpty) {
-                  this += lineBreak
-                  printTrees(stats, lineBreak)
-                }
-                this += lineBreak
-                printTree(expr)
+        val parents1 = parents.filter {
+          case Term.Apply(Term.Select(Term.New(tpt), _, _), _) if Types.isJavaLangObject(tpt.tpe) => false
+          case TypeTree.TypeSelect(Term.Select(Term.Ident("_root_"), "scala", _), "Product") => false
+          case _ => true
+        }
+        if (parents1.nonEmpty) {
+          sb.append(" extends")
+          parents1.foreach {
+            case parent@Term.Apply(Term.Select(Term.New(tpt), _, _), args) =>
+              this += " "
+              printTypeTree(tpt)
+              if (args.nonEmpty) {
+                this += "("
+                printTrees(args, ", ")
+                this += ")"
               }
-              this += lineBreak += "}"
+
+            case parent@TypeTree() =>
+              sb.append(" ")
+              printTypeTree(parent)
           }
+        }
 
-        case Term.Inlined(call, bindings, expansion) =>
-          sb.append("{ // inlined")
-          indented {
-            if (!bindings.isEmpty) {
-              this += lineBreak
-              printTrees(bindings, lineBreak)
-            }
-            this += lineBreak
-            printTree(expansion)
-          }
-          this += lineBreak
-          this += "}"
-
-        case Term.Lambda(meth, tpt) =>
-        // Printed in Term.Block branch
-
-        case Term.If(cond, thenp, elsep) =>
-          this += "if ("
-          printTree(cond)
-          this += ") "
-          printTree(thenp)
-          this += " else "
-          printTree(elsep)
-
-        case Term.Match(selector, cases) =>
-          printTree(selector)
-          this += " match {"
+        val stats1 = stats.collect {
+          case stat@Definition() if !stat.flags.isParam && !stat.flags.isParamAccessor => stat
+          case stat@Import(_, _) => stat
+          case stat@Term() => stat
+        }
+        if (stats1.nonEmpty) {
+          this += " {"
           indented {
             this += lineBreak
-            printCases(cases, lineBreak)
+            printTrees(stats1, lineBreak)
           }
           this += lineBreak += "}"
+        }
+        this
 
-        case Term.Try(_, _, _) =>
-          ???
+      case tdef@TypeDef(name, rhs) =>
+        this += "type "
+        printTargDef(tdef)
 
-        case Term.Return(_) =>
-          ???
+      case vdef@ValDef(name, tpt, rhs) =>
+        val flags = vdef.flags
+        if (flags.isOverride) this += "override "
 
-        case Term.Repeated(elems) =>
-          printTrees(elems, ", ")
+        if (flags.isLazy) this += "lazy "
+        if (vdef.flags.isMutable) this += "var "
+        else this += "val "
 
-        case Term.SelectOuter(_, _, _) =>
-          ???
+        this += name += ": "
+        printTypeTree(tpt)
+        rhs match {
+          case Some(tree) =>
+            this += " = "
+            printTree(tree)
+          case None =>
+        }
+        this
 
-      }
-      this
+      case ddef@DefDef(name, targs, argss, tpt, rhs) =>
+        val flags = ddef.flags
+        if (flags.isOverride) sb.append("override ")
+
+        this += "def " += name
+        printTargsDefs(targs)
+        val it = argss.iterator
+        while (it.hasNext)
+          printArgsDefs(it.next())
+        this += ": "
+        printTypeTree(tpt)
+        rhs match {
+          case Some(tree) =>
+            this += " = "
+            printTree(tree)
+          case None =>
+        }
+        this
+
+      case tree@Term.Ident(name) =>
+        printType(tree.tpe)
+
+      case Term.Select(qual, name, sig) =>
+        printTree(qual)
+        if (name != "<init>")
+          this += "." += name
+        this
+
+      case Term.Literal(const) =>
+        printConstant(const)
+
+      case Term.This(id) =>
+        this += "this" // TODO add id
+
+      case Term.New(tpt) =>
+        this += "new "
+        printTypeTree(tpt)
+
+      case Term.NamedArg(name, arg) =>
+        ???
+
+      case SpecialOp("throw", expr :: Nil) =>
+        this += "throw "
+        printTree(expr)
+
+      case Term.Apply(fn, args) =>
+        printTree(fn)
+        this += "("
+        printTrees(args, ", ")
+        this += ")"
+
+      case Term.TypeApply(fn, args) =>
+        printTree(fn)
+        this += "["
+        printTypeTrees(args, ", ")
+        this += "]"
+
+      case Term.Super(qual, tptOpt) =>
+        ???
+
+      case Term.Typed(term, tpt) =>
+        this += "("
+        printTree(term)
+        this += ": "
+        printTypeTree(tpt)
+        this += ")"
+
+      case Term.Assign(lhs, rhs) =>
+        printTree(lhs)
+        this += " = "
+        printTree(rhs)
+
+      case Term.Block(stats, expr) =>
+        expr match {
+          case Term.Lambda(_, _) =>
+            // Decompile lambda from { def annon$(...) = ...; closure(annon$, ...)}
+            val DefDef(_, _, args :: Nil, _, Some(rhs)) :: Nil = stats
+            this += "("
+            printArgsDefs(args)
+            this += " => "
+            printTree(rhs)
+            this += ")"
+
+          case Term.Apply(Term.Ident("while$"), _) =>
+            val DefDef("while$", _, _, _, Some(Term.If(cond, Term.Block(body :: Nil, _), _))) = stats.head
+            this += "while ("
+            printTree(cond)
+            this += ") "
+            printTree(body)
+
+          case Term.Apply(Term.Ident("doWhile$"), _) =>
+            val DefDef("doWhile$", _, _, _, Some(Term.Block(List(body), Term.If(cond, _, _)))) = stats.head
+            this += "do "
+            printTree(body)
+            this += " while ("
+            printTree(cond)
+            this += ")"
+
+          case _ =>
+            this += "{"
+            indented {
+              if (!stats.isEmpty) {
+                this += lineBreak
+                printTrees(stats, lineBreak)
+              }
+              this += lineBreak
+              printTree(expr)
+            }
+            this += lineBreak += "}"
+        }
+
+      case Term.Inlined(call, bindings, expansion) =>
+        sb.append("{ // inlined")
+        indented {
+          if (!bindings.isEmpty) {
+            this += lineBreak
+            printTrees(bindings, lineBreak)
+          }
+          this += lineBreak
+          printTree(expansion)
+        }
+        this += lineBreak += "}"
+
+      case Term.Lambda(meth, tpt) =>
+        // Printed in Term.Block branch
+        this
+
+      case Term.If(cond, thenp, elsep) =>
+        this += "if ("
+        printTree(cond)
+        this += ") "
+        printTree(thenp)
+        this+= " else "
+        printTree(elsep)
+
+      case Term.Match(selector, cases) =>
+        printTree(selector)
+        this += " match {"
+        indented {
+          this += lineBreak
+          printCases(cases, lineBreak)
+        }
+        this += lineBreak += "}"
+
+      case Term.Try(_, _, _) =>
+        ???
+
+      case Term.Return(_) =>
+        ???
+
+      case Term.Repeated(elems) =>
+        printTrees(elems, ", ")
+
+      case Term.SelectOuter(_, _, _) =>
+        ???
+
     }
 
-
-
-    def printTrees(trees: List[Tree], sep: String): Unit = {
+    def printTrees(trees: List[Tree], sep: String): Buffer = {
       def printSeparated(list: List[Tree]): Unit = list match {
         case Nil =>
         case x :: Nil => printTree(x)
@@ -292,11 +290,11 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
           this += sep
           printSeparated(xs)
       }
-
       printSeparated(trees)
+      this
     }
 
-    def printCases(cases: List[CaseDef], sep: String): Unit = {
+    def printCases(cases: List[CaseDef], sep: String): Buffer = {
       def printSeparated(list: List[CaseDef]): Unit = list match {
         case Nil =>
         case x :: Nil => printCase(x)
@@ -305,11 +303,11 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
           this += sep
           printSeparated(xs)
       }
-
       printSeparated(cases)
+      this
     }
 
-    def printTypeTrees(typesTrees: List[TypeTree], sep: String): Unit = {
+    def printTypeTrees(typesTrees: List[TypeTree], sep: String): Buffer = {
       def printSeparated(list: List[TypeTree]): Unit = list match {
         case Nil =>
         case x :: Nil => printTypeTree(x)
@@ -318,11 +316,11 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
           this += sep
           printSeparated(xs)
       }
-
       printSeparated(typesTrees)
+      this
     }
 
-    def printTypesOrBounds(types: List[TypeOrBounds], sep: String): Unit = {
+    def printTypesOrBounds(types: List[TypeOrBounds], sep: String): Buffer = {
       def printSeparated(list: List[TypeOrBounds]): Unit = list match {
         case Nil =>
         case x :: Nil => printTypeOrBound(x)
@@ -331,8 +329,8 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
           this += sep
           printSeparated(xs)
       }
-
       printSeparated(types)
+      this
     }
 
     def printTargsDefs(targs: List[TypeDef]): Unit = {
@@ -352,19 +350,19 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
       }
     }
 
-    def printTargDef(arg: TypeDef): Unit = {
+    def printTargDef(arg: TypeDef): Buffer = {
       val TypeDef(name, rhs) = arg
-      sb.append(name)
+      this += name
       rhs match {
         case TypeBoundsTree(lo, hi) =>
           lo match {
-            case TypeTree.Synthetic() =>
+            case TypeTree.Synthetic() => this
             case _ =>
               this += " >: "
               printTypeTree(lo)
           }
           hi match {
-            case TypeTree.Synthetic() =>
+            case TypeTree.Synthetic() => this
             case _ =>
               this += " <: "
               printTypeTree(hi)
@@ -397,7 +395,7 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
       printTypeTree(tpt)
     }
 
-    def printCase(caseDef: CaseDef): Unit = {
+    def printCase(caseDef: CaseDef): Buffer = {
       val CaseDef(pat, guard, body) = caseDef
       this += "case "
       printPattern(pat)
@@ -407,13 +405,13 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
         this += lineBreak
         printTree(body)
       }
-
+      this
     }
 
-    def printPattern(pattern: Pattern): Unit = pattern match {
+    def printPattern(pattern: Pattern): Buffer = pattern match {
       case Pattern.Value(v) =>
         v match {
-          case Term.Ident("_") => sb.append("_")
+          case Term.Ident("_") => this += "_"
           case _ => printTree(v)
         }
 
@@ -446,12 +444,9 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
       case Constant.String(v) => this += '"' += v.toString += '"' // TODO escape string
     }
 
-    def printTypeOrBoundsTree(tpt: TypeOrBoundsTree): Buffer = {
-      tpt match {
-        case TypeBoundsTree(lo, hi) => ???
-        case tpt@Type() => printType(tpt)
-      }
-      this
+    def printTypeOrBoundsTree(tpt: TypeOrBoundsTree): Buffer = tpt match {
+      case TypeBoundsTree(lo, hi) => ???
+      case tpt@Type() => printType(tpt)
     }
 
     def printTypeTree(tree: TypeTree): Buffer = {
@@ -463,8 +458,7 @@ class ShowSourceCode[T <: Tasty with Singleton](tasty0: T) extends Show[T](tasty
           printType(tree.tpe)
 
         case TypeTree.TypeSelect(qual, name) =>
-          printTree(qual)
-          this += "." += name
+          printTree(qual) += "." += name
 
         case TypeTree.Singleton(_) =>
           ???
