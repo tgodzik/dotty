5ed9f171e7f5ecce3050f5a1687971a49b62a4fa
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2018-06-13 14:00:20 +0200

    Decompose precise protos during typing

--- tests/pos/transparent.scala
+++ tests/pos/transparent.scala
@@ -31,18 +31,21 @@ object ITE {
   // foo(false): { if(false) 1 else 2 }
   // var b: Boolean = true
   // foo(b): { if(b) 1 else 2 }
+
+  transparent def foo2(b: Boolean): { if (b) 1 else 2 } =
+    if (b) 1 else 2
 }
 
-object Match {
-  transparent def foo(b: Boolean) = {
-    val res = b match {
-      case true => 1
-      case false => 2
-    }
-    identity[{ b match { case true => 1; case false => 2 } }](res)
-    res
-  }
-}
+// object Match {
+//   transparent def foo(b: Boolean) = {
+//     val res = b match {
+//       case true => 1
+//       case false => 2
+//     }
+//     identity[{ b match { case true => 1; case false => 2 } }](res)
+//     res
+//   }
+// }
 
 // object AvoidLocalRefs {
 //   type Id[T] = T
