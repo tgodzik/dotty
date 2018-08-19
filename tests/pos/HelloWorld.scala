29565f41d9b81c2db964ca972afba0cb0f8ef194
Sébastien Doeraene <sjrdoeraene@gmail.com>
2018-08-19 11:19:10 +0200

    wip

--- tests/pos/HelloWorld.scala
+++ tests/pos/HelloWorld.scala
@@ -1,3 +1,15 @@
 object HelloWorld {
-  def main(args: Array[String]): Unit = println("hello world")
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+    val list = List(1, 2, 3)
+    list match {
+      case head :: tail => println(head)
+      case Nil => println("empty")
+    }
+    val s = list match {
+      case head :: tail => head.toString
+      case Nil => "empty"
+    }
+    println(s)
+  }
 }
