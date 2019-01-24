scala/scala3

Fix #5503: Keep outermost inlined call

compiler/src/dotty/tools/dotc/transform/PostTyper.scala
@@ -239,8 +239,10 @@ class PostTyper extends MacroTransform with IdentityDenotTransformer { thisPhase
               super.transform(tree1)
           }
         case Inlined(call, bindings, expansion) if !call.isEmpty =>
-          val callTrace = Inliner.inlineCallTrace(call.symbol, call.pos)
-          cpy.Inlined(tree)(callTrace, transformSub(bindings), transform(expansion)(inlineContext(call)))
+          val callTrace =
+            if (!isInInlinedTree) Inliner.outlineCallTrace(call)
+            else Inliner.inlineCallTrace(call.symbol, call.pos)
+          cpy.Inlined(tree)(callTrace, transformSub(bindings)(inlinedTreeContext), transform(expansion)(inlineContext(call)))
         case tree: Template =>
           withNoCheckNews(tree.parents.flatMap(newPart)) {
             val templ1 = paramFwd.forwardParamAccessors(tree)

compiler/src/dotty/tools/dotc/transform/Staging.scala
@@ -620,6 +620,9 @@ class Staging extends MacroTransformWithImplicits {
                   """.stripMargin, tree.rhs.pos)
                 EmptyTree
             }
+          case tree @ Inlined(call, bindings, expansion) if !call.isEmpty && inQuote =>
+            // Remove inline call (and keep trace) of quoted trees that will be pickled
+            super.transform(cpy.Inlined(tree)(Inliner.inlineCallTrace(call.symbol, call.pos), bindings, expansion))
           case _ =>
             markDef(tree)
             checkLevel(mapOverTree(enteredSyms))

tgodzik/dotty

compiler/src/dotty/tools/dotc/transform/PostTyper.scala
@@ -244,8 +244,10 @@ class PostTyper extends MacroTransform with IdentityDenotTransformer { thisPhase
               super.transform(tree1)
           }
         case Inlined(call, bindings, expansion) if !call.isEmpty =>
-          val callTrace = Inliner.inlineCallTrace(call.symbol, call.sourcePos)
-          cpy.Inlined(tree)(callTrace, transformSub(bindings), transform(expansion)(inlineContext(call)))
+          val callTrace =
+            if (!isInInlinedTree) Inliner.outlineCallTrace(call)
+            else Inliner.inlineCallTrace(call.symbol, call.sourcePos)
+          cpy.Inlined(tree)(callTrace, transformSub(bindings)(inlinedTreeContext), transform(expansion)(inlineContext(call)))
         case tree: Template =>
           withNoCheckNews(tree.parents.flatMap(newPart)) {
             val templ1 = paramFwd.forwardParamAccessors(tree)

compiler/src/dotty/tools/dotc/transform/Staging.scala
@@ -622,6 +622,9 @@ class Staging extends MacroTransformWithImplicits {
                   """.stripMargin, tree.rhs.sourcePos)
                 EmptyTree
             }
+          case tree @ Inlined(call, bindings, expansion) if !call.isEmpty && inQuote =>
+            // Remove inline call (and keep trace) of quoted trees that will be pickled
+            super.transform(cpy.Inlined(tree)(Inliner.inlineCallTrace(call.symbol, call.sourcePos), bindings, expansion))
           case _ =>
             markDef(tree)
             checkLevel(mapOverTree(enteredSyms))