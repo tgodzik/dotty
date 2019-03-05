8f04ba5a3b01622cb4e4fad3dc7aa72f0791da49
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-03-05 11:50:01 +0100

    Simplify ReflectionImpl

--- compiler/src/dotty/tools/dotc/tastyreflect/ReflectionImpl.scala
+++ compiler/src/dotty/tools/dotc/tastyreflect/ReflectionImpl.scala
@@ -3,19 +3,14 @@ package dotty.tools.dotc.tastyreflect
 import dotty.tools.dotc.core._
 import dotty.tools.dotc.util.{SourcePosition, Spans}
 
-class ReflectionImpl private (ctx: Contexts.Context, pos: SourcePosition)
-    extends scala.tasty.Reflection {
-
-  val kernel: KernelImpl = new KernelImpl(ctx, pos)
-
-}
-
 object ReflectionImpl {
 
-  def apply(rootContext: Contexts.Context): ReflectionImpl =
+  def apply(rootContext: Contexts.Context): scala.tasty.Reflection { val kernel: KernelImpl } =
     apply(rootContext, SourcePosition(rootContext.source, Spans.NoSpan))
 
-  def apply(rootContext: Contexts.Context, rootPosition: SourcePosition): ReflectionImpl =
-    new ReflectionImpl(rootContext, rootPosition)
+  def apply(rootContext: Contexts.Context, rootPosition: SourcePosition): scala.tasty.Reflection { val kernel: KernelImpl } = {
+    class ReflectionImpl(val kernel: KernelImpl) extends scala.tasty.Reflection
+    new ReflectionImpl(new KernelImpl(rootContext, rootPosition))
+  }
 
 }
