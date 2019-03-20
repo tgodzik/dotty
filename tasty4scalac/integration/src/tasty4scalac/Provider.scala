cac947f8706e88d0d538127ddbfd6744714853e7
Marek Żarnowski <mzarnowski@virtuslab.com>
2019-03-20 12:57:05 +0100

    do not fail when there are no test cases

--- tasty4scalac/integration/src/tasty4scalac/Provider.scala
+++ tasty4scalac/integration/src/tasty4scalac/Provider.scala
@@ -32,7 +32,8 @@ object Providers {
     }
 
     private def findTestCases(path: Path): Seq[Path] = {
-      Files.walk(path)
+      if (!Files.exists(path)) Nil
+      else Files.walk(path)
         .iterator()
         .asScala
         .filterNot(path => negativeTests.exists(p => p.matcher(path.toString).matches()))
